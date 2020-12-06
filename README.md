31a281059050ab72c5cd12156e91d4401ec21efc
Cemal <cc.isci@hotmail.com>
2020-12-06 13:24:12 +0300

    complete

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# mevnAuthCli
-mevnAuthCli
+# mevn-auth-cli
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Login.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <div class="row">
+      <div class="card mx-auto">
+        <div class="card-header text-white bg-primary">
+          <h4>Login</h4>
+        </div>
+        <div class="card-body">
+          <form @click.prevent="loginUser">
+            <div class="form-group">
+              <label for="username">Username</label>
+              <input
+                id="username"
+                placeholder="Username"
+                name="username"
+                v-model="username"
+                type="text"
+                class="form-control"
+              />
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                placeholder="Password"
+                name="password"
+                id="password"
+                v-model="password"
+              />
+            </div>
+            <button class="btn btn-primary">Login</button>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <router-link to="/register" class="card-link"
+              >Need and account?</router-link
+            >
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapActions } from "vuex";
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    ...mapActions(["login"]),
+    loginUser() {
+      const { username, password, login } = this;
+      let user = {
+        username,
+        password,
+      };
+      login(user)
+        .then((res) => {
+          if (res.data.success) {
+            this.$router.push("/profile");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
+<style>
+.card {
+  width: 60%;
+  border-radius: 0;
+}
+.btn {
+  border-radius: 0;
+}
+.form-control {
+  border-radius: 0;
+}
+</style>
